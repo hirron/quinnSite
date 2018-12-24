5693a006b830419be02bd66cbc646a488cc83f9c
quinnpat <quinnpaterson_1996@yahoo.ca>
2018-12-24 01:38:51 -0800

    added alternate routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/Dad', function(req, res, next) {
+  res.render('index', { title: 'Dad' });
+});
+
+router.get('/Dee', function(req, res, next) {
+  res.render('index', { title: 'Dee' });
+});
+
+router.get('/Sam', function(req, res, next) {
+  res.render('index', { title: 'Sam' });
+});
+
+router.get('/Grandma', function(req, res, next) {
+  res.render('index', { title: 'Grandma' });
+});
+
 module.exports = router;
