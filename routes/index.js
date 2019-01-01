7522fb5b317794ea6aceb4d19fb4d908469019bd
quinnpat <quinnpaterson_1996@yahoo.ca>
2019-01-01 14:44:24 -0800

    updated get request

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,7 @@ router.get('/goals', function(req, res, next) {
          message = "fail"
          res.status(404).send(err);
      }
-     message = client.db("goals").collection("nextYear").find();
+     message = JSON.stringify(client.db("goals").collection("nextYear").find());
 
      res.status(200).send(message);
      client.close();
