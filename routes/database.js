3f6d99b9712ecc783faf35f6cd2453250d03ddef
quinnpat <quinnpaterson_1996@yahoo.ca>
2018-12-31 22:18:01 -0800

    hello

--- routes/database.js
+++ routes/database.js
@@ -1,32 +1,6 @@
-var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 
 
 
 var router = express.Router();
-var uri = "mongodb://quinn:2018@goals-shard-00-00-9yjnu.mongodb.net:27017,goals-shard-00-01-9yjnu.mongodb.net:27017,goals-shard-00-02-9yjnu.mongodb.net:27017/test?ssl=true&replicaSet=goals-shard-0&authSource=admin&retryWrites=true";
-
-
-router.get('/goals', function(req, res, next) {
-   const collection;
-  MongoClient.connect(uri, function(err, client) {
-
-     const collection = client.db("goals").collection("nextYear");
-
-     client.close();
-  });
-   res.status(200).send(collection);
-});
-
-router.post('/goals', function(req, res, next) {
-  MongoClient.connect(uri, function(err, client) {
-     client.db("goals").collection("nextYear").insertOne({
-            goal: req.param('message')
-     });
-
-     client.close();
-  });
-  res.status(200).send("this worked");
-});
-
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,5 +24,32 @@ router.get('/Sam', function(req, res, next) {
 router.get('/Grandma', function(req, res, next) {
   res.render('index', { title: 'Grandma' });
 });
+
+var uri = "mongodb://quinn:2018@goals-shard-00-00-9yjnu.mongodb.net:27017,goals-shard-00-01-9yjnu.mongodb.net:27017,goals-shard-00-02-9yjnu.mongodb.net:27017/test?ssl=true&replicaSet=goals-shard-0&authSource=admin&retryWrites=true";
+
+
+router.get('/goals', function(req, res, next) {
+   const collection;
+  MongoClient.connect(uri, function(err, client) {
+
+     const collection = client.db("goals").collection("nextYear");
+
+     client.close();
+  });
+   res.status(200).send(collection);
+});
+
+router.post('/goals', function(req, res, next) {
+  MongoClient.connect(uri, function(err, client) {
+     client.db("goals").collection("nextYear").insertOne({
+            goal: req.param('message')
+     });
+
+     client.close();
+  });
+  res.status(200).send("this worked");
+});
+
+
 
 module.exports = router;
