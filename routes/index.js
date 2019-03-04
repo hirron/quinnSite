96f2c209ca11c0c89ecd2d3cc19eca762316f371
Quinn Paterson <quinnpaterson_1996@yahoo.ca>
2019-03-03 22:30:52 -0800

    added content to page, tryed to resolve root issue

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/download/myResume', function(req, res){
 });
 
 router.get('/download/resume', function(req, res){
-  var file = appRoot + '/upload-folder/resume.pdf';
+  var file = appRoot + '/images/resume.pdf';
   res.download(file); // Set disposition and send it.
 });
 
